2178f3ee388272620377cd9e1f2cd67609b4b971
Igor S. Morozov <im@planado.ru>
2018-01-29 19:38:06 +0300

    Jade -> pug

--- scripts/setup-cache.js
+++ scripts/setup-cache.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 
 var theme;
-var fs = require('fs');
+var fs = require("fs");
 
 try {
-  theme = require('../lib/main');
+  theme = require("../lib/main");
 } catch (err) {
-  require('coffee-script/register');
-  theme = require('../src/main');
+  require("coffeescript/register");
+  theme = require("../src/main");
 }
 
-if (!fs.existsSync('./cache')) {
-  fs.mkdirSync('./cache');
+if (!fs.existsSync("./cache")) {
+  fs.mkdirSync("./cache");
 }
 
-fs.chmodSync('./cache', 0777);
+fs.chmodSync("./cache", 0777);
 
 // Call with known options to generate the cache entries. This isn't that
 // efficient but it works for now.
-theme.render({}, function () {});
-theme.render({}, {themeVariables: 'flatly'}, function () {});
-theme.render({}, {themeVariables: 'slate'}, function () {});
-theme.render({}, {themeVariables: 'cyborg'}, function () {});
+theme.render({}, function() {});
+theme.render({}, { themeVariables: "flatly" }, function() {});
+theme.render({}, { themeVariables: "slate" }, function() {});
+theme.render({}, { themeVariables: "cyborg" }, function() {});
